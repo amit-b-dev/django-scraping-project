117cb327fa1ae4d190fd72123fd20d2d023c6f6b
amit18-dev <amit.online18@gmail.com>
2026-01-13 19:02:57 +0530

    case type api added gujarat and bombay

--- bombay_high_court/urls.py
+++ bombay_high_court/urls.py
@@ -3,7 +3,7 @@ from .high_court.highCourt_Scrape import highCourt_API, caseSideList_API, caseTy
 
 urlpatterns = [
     path('b1/hc-bombay/cases/case-no', highCourt_API, name="highCourt_API"),
-    path('b1/hc-gujrat/case-side-list', caseSideList_API, name="caseSideList_API"),
-    path('b1/hc-gujrat/case-stam-reg-list', stampAndRegList_API, name="stampAndRegList_API"),
-    path('b1/hc-gujrat/case-type-list', caseTypeList_API, name="caseTypeList_API"),
+    path('b1/hc-bombay/case-side-list', caseSideList_API, name="caseSideList_API"),
+    path('b1/hc-bombay/case-stam-reg-list', stampAndRegList_API, name="stampAndRegList_API"),
+    path('b1/hc-bombay/case-type-list', caseTypeList_API, name="caseTypeList_API"),
 ]
